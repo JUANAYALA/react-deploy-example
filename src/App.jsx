5e5ebbe65ea5ba073e1dd2c141552d4766e18dea
Juan Ayala <juanpabloayalaarboleda@gmail.com>
2023-04-12 21:26:43 -0500

    despliegue

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,13 @@ function App() {
     <main className="bg-slate-600 h-screen">
       <div className="container mx-auto">
       <br />
-        <h1 className="text-center text-4xl">Seccion de crear tarea</h1>
+        <h1 className="text-center text-4xl">Seccion de crear novedad</h1>
         <br />
         <TaskForm />
         <br />
         <hr />
         <br />
-        <h1 className="text-center text-4xl">Seccion de tarea</h1>
+        <h1 className="text-center text-4xl">Seccion de Novedad</h1>
         <br />
         <TaskList />
       </div>
